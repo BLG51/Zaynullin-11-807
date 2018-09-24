424b228089c3325c28fec4833bac99699898570c
BLG51 <zary1998@gmail.com>
2018-09-25 00:17:49 +0300

    hw6 done

---
+++ Homework06/src/Homework02.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Homework02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(spaceDel(s));
+    }
+
+    public static String spaceDel(String s) {
+        //return s.trim();
+        char[] arr = s.toCharArray();
+        int i = 0;
+        while (arr[i] == ' ') {
+            i++;
+        }
+        return s.substring(i);
+    }
+}
