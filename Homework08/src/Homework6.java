6079995e38474e171bed2617ab3f69a0ef6e80f0
BLG51 <zary1998@gmail.com>
2018-10-02 00:48:35 +0300

    Hw8* done

--- Homework08/src/Homework6.java
+++ Homework08/src/Homework6.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
 
-public class Homework3 {
+public class Homework6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int m = n; //sc.nextInt();
-        int c = n * m, i = 0, j = -1;
+        int m = sc.nextInt();
+        int c = (((n * m) - n) / 2) + n, i = 0, j = -1;
+        //System.out.println(c);
         int[][] matrix = new int[n][m];
         int k = 0;
         while (k < c) {
@@ -19,12 +20,8 @@ public class Homework3 {
                 k++;
                 matrix[i][j] = k;
             }
-            while (j - 1 > -1 && matrix[i][j - 1] == 0) {
+            while (j - 1 > -1 && i - 1 > -1 && matrix[i - 1][j - 1] == 0) {
                 j--;
-                k++;
-                matrix[i][j] = k;
-            }
-            while (i - 1 > -1 && matrix[i - 1][j] == 0) {
                 i--;
                 k++;
                 matrix[i][j] = k;
