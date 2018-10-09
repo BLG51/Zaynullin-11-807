e31ca2e54a1d9e987b2e8f00ea63870848a62600
BLG51 <zary1998@gmail.com>
2018-10-09 12:35:23 +0300

    2nd done

--- СlassTest01/src/ClassTestTask02.java
+++ СlassTest01/src/ClassTestTask02.java
@@ -4,9 +4,16 @@ public class ClassTestTask02 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
-        char[] arr = s.toCharArray();
-        //for (int i = 0; i <arr.length i++) {
-
-    //    }
+        int sum = s.charAt(0) - 48;
+        for (int i = 1; i < s.length(); i = i + 2) {
+            switch (s.charAt(i)) {
+                case '+':
+                    sum += s.charAt(i + 1) - 48;
+                    break;
+                case '-':
+                    sum -= s.charAt(i + 1) - 48;
+            }
+        }
+        System.out.println(sum);
     }
 }
