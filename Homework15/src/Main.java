0f9f8fae75659dcfd25ee927c86154f6669059ca
BLG51 <zary1998@gmail.com>
2018-11-16 14:24:14 +0300

    some fixes

--- Homework15/src/Main.java
+++ Homework15/src/Main.java
@@ -11,8 +11,8 @@ public class Main {
         String s2 = "Я совсем ничего не делал";
         double coef1 = a.analyze(new SimpleTextProvider(s1), new SimpleTextProvider(s2));
         double coef12 = b.analyze(new SimpleTextProvider(s1), new SimpleTextProvider(s2));
-        System.out.println("Жаккард для просто строк: " + coef1);
-        System.out.println("Косинусная мера для просто строк: " + coef12);
+        System.out.printf("Жаккард для просто строк: %.3f \n", coef1);
+        System.out.printf("Косинусная мера для просто строк: %.3f \n", coef12);
 
         //Тоже вычисляем коэффициент, только сравниваем текст, хранящийся в двух файлах.
         //Текст в файлах точно такой же как при сравнении выше, поэтому результаты одинаковые
@@ -22,8 +22,8 @@ public class Main {
         File f2 = createFile("f2.txt", s2);
         double coef2 = a.analyze(new FileTextProvider(f1), new FileTextProvider(f2));
         double coef22 = b.analyze(new FileTextProvider(f1), new FileTextProvider(f2));
-        System.out.println("Жаккард для содержимого файлов: " + coef2);
-        System.out.println("Косинусная мера для содержимого файлов: " + coef22);
+        System.out.printf("Жаккард для содержимого файлов: %.3f \n", coef2);
+        System.out.printf("Косинусная мера для содержимого файлов: %.3f \n", coef22);
         f1.delete();
         f2.delete();
     }
